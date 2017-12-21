cad62e2d11bd45b564fc9c8ae4de368f182e68cc
Adalen Vladi <adalenvladi@gmail.com>
2017-12-21 16:31:04 +0100

    js functions

--- js/index.js
+++ js/index.js
@@ -1,3 +1,98 @@
+//made by Adalen VLADI
+
+
+app={
+	var:{
+		user:'wpuser',
+		currency:'bitcoin',
+		last_msg_id:'3'
+	},
+
+	function:{
+		firstRun:function(currency){
+			$.ajax({
+				url: 'back/',
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					currency: currency,
+					function:'first_run'
+				}
+			})
+			.done(function(data) {
+				console.log(data);
+				app.function.displayMessages(data,'first_run');
+			})
+			.fail(function() {
+				console.log("error");
+			})			
+		},//firstrun end
+
+		getMessages:function(currency){
+			$.ajax({
+				url: 'back/',
+				type: 'POST',
+				dataType: 'json',
+				data: {
+					currency: currency,
+					function:'get_messages',
+					last_msg_id:app.var.last_msg_id
+				}
+			})
+			.done(function(data) {
+				if(typeof data !== 'undefined'){
+					console.log(data);
+					app.var.last_msg_id=data[data.length-1].id;
+					app.function.displayMessages(data,'new');
+				}
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			
+		},//getMessage end
+
+		displayMessages:function(data,type){
+
+					if (type=='first_run') {
+						$('.chat-history').html('');		
+					}	
+					for (var i = 0; i < data.length; i++) {
+						$('.chat-history').append('<div class="chat-message clearfix">'+
+								
+								'<img src="'+data[i].avatar+'" alt="" width="32" height="32">'+
+
+								'<div class="chat-message-content clearfix">'+
+									
+									'<span class="chat-time">'+data[i].datetime+'</span>'+
+
+									'<h5>'+data[i].user+'</h5>'+
+
+									'<p>'+data[i].text+'</p>'+
+
+								'</div> '+
+
+							'</div>'+
+
+						'<hr>');
+					}//for end
+
+			
+
+		},//display messages end
+
+		setCurrency:function(currency){
+			app.var.currency=currency;
+			app.var.last_msg_id='null';
+		}//setcurrency end
+
+	}
+
+
+
+};
+
+
 (function() {
 
 	$('#live-chat header').on('click', function() {
@@ -13,5 +108,7 @@
 		$('#live-chat').fadeOut(300);
 
 	});
+
+	app.function.firstRun(app.var.currency);
 
 }) ();
