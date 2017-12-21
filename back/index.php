ea685481681baad1d8652d03272708a685b7375f
Adalen Vladi <adalenvladi@gmail.com>
2017-12-21 16:30:33 +0100

    backend php api

---
+++ back/index.php
@@ -0,0 +1,43 @@
+<?php 
+$con=mysqli_connect("127.0.0.1", "root", "", "cryptoChat");
+
+$function=mysqli_real_escape_string($con,$_POST['function']);
+$currency=mysqli_escape_string($con,$_POST['currency']);
+$json=array();
+
+switch ($function) {
+	case 'get_messages':
+		$last_msg_id=mysqli_escape_string($con,$_POST['last_msg_id']);
+
+		if ($last_msg_id!='null') {
+			$data=mysqli_query($con,"SELECT * FROM messages WHERE currency ='".$currency."' AND id > '".$last_msg_id."' ");
+		} else{
+			$data=mysqli_query($con,"SELECT * FROM messages WHERE currency ='".$currency."' ");
+		}
+
+
+		while($row =mysqli_fetch_assoc($data))
+		{
+		    $json[] = $row;
+		}
+		 echo(json_encode($json));
+
+	break;
+	case 'first_run':
+		
+		$data=mysqli_query($con,'SELECT * FROM messages WHERE currency ="'.$currency.'" ');
+
+		while($row =mysqli_fetch_assoc($data))
+		{
+		    $json[] = $row;
+		}
+		 echo(json_encode($json));
+
+		break;
+	
+	default:
+		# code...
+		break;
+}
+
+?>
